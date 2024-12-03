bd0c598d346ad7edd06ddcbc655aa2c2fb63deb5
duygudumlupinar <87421564+duygudumlupinar@users.noreply.github.com>
2024-12-03 23:20:09 +0300

    Intro and login page bases

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import "./components/Login";
+import Login from "./components/Login";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Quiz</div>;
+  return (
+    <div className="backgroundIntroPage">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import React from "react";
+
+const Login = () => {
+  return <div className="loginArea">Login</div>;
+};
+
+export default Login;
