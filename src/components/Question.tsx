563ead2b3ed21b066d676945954a12dde60019bf
duygudumlupinar <87421564+duygudumlupinar@users.noreply.github.com>
2024-12-18 21:54:40 +0300

    Questions

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Question = () => {
-  return <div className="backgroundQuestion">Question</div>;
+  return (
+    <div className="backgroundQuestion">
+      <div className="descriptionText">Text question</div>
+      <div className="optionsArea">
+        <span className="optionButton">option</span>
+        <span className="optionButton">option</span>
+        <span className="optionButton">option</span>
+        <span className="optionButton">option</span>
+      </div>
+      <div>
+        <button className="mainButton">Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default Question;
